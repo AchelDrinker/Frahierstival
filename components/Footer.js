30eb3f5e43b2cb269ab0f299d0be3adbed811b60
AchelDrinker <hmartineu@gmail.com>
2025-12-16 22:07:42 +0100

    correction

--- components/Footer.js
+++ components/Footer.js
@@ -10,7 +10,7 @@ const Footer = (props) => {
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
         <ul className="icons">
             <li><a href="https://www.facebook.com/p/Frahierstival-61573934457910/" target="_blank" rel="noopener noreferrer" className="icon"><FontAwesomeIcon icon={faFacebook} /></a></li>
-            <li><a href="https://www.instagram.com/frahierstival_2026/" target="_blank" rel="noopener noreferrer" className="icon"><FontAwesomeIcon icon={faInstagram} /></a></li>
+            <li><a href="https://www.instagram.com/frahierstival/" target="_blank" rel="noopener noreferrer" className="icon"><FontAwesomeIcon icon={faInstagram} /></a></li>
         </ul>
         <p className="copyright">&copy; Frahier'stival. {t.design}: <a href="https://www.linkedin.com/in/hugomartineu/">Hugo Martineu</a>.</p>
     </footer>
